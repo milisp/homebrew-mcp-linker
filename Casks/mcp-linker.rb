c127f5bad8a1387ac57e06043abfb1a5ac84f74b
milisp <milisp@proton.me>
2026-01-06 09:40:48 -0500

    Update mcp-linker cask to version 2.0.1, including updated SHA256 checksums for Intel and ARM architectures

--- Casks/mcp-linker.rb
+++ Casks/mcp-linker.rb
@@ -1,14 +1,14 @@
 cask "mcp-linker" do
-  version "2.0.0"
+  version "2.0.1"
 
   on_intel do
-    sha256 "24e17fead786b206f035291b24becbbc7adf2f2ee109a920b715e6589bb17000"
+    sha256 "cd59041020b654d53e78766831ab3e24fad32b80979a0c24917fbbba2519ab90"
 
     url "https://github.com/milisp/mcp-linker/releases/download/v#{version}/MCPLinker_#{version}_x64.dmg"
   end
 
   on_arm do
-    sha256 "c0d4ae710fed0778410c69b7f05d3538c0e4116cd2550c950d852ac73e19e43b"
+    sha256 "49e506166de4d442efbff83f3cca1c4afde4407b28261b82de596ab981a596b1"
 
     url "https://github.com/milisp/mcp-linker/releases/download/v#{version}/MCPLinker_#{version}_aarch64.dmg"
   end
