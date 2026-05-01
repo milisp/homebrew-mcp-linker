f87ed6c67b813b64cd17a29d9b236eb2be172cbe
milisp <milisp@proton.me>
2026-05-01 03:38:38 -0400

    Update mcp-linker cask to version 2.2.0

--- Casks/mcp-linker.rb
+++ Casks/mcp-linker.rb
@@ -1,14 +1,14 @@
 cask "mcp-linker" do
-  version "2.1.0"
+  version "2.2.0"
 
   on_intel do
-    sha256 "2ba6493c3c7f28afe82db541d97cb74ca336232ce31bf64f08579910bf144a08"
+    sha256 "e1e5e0091f1500496bded468230b8147fe98e88a879f13ebb0e058d07a5799b4"
 
     url "https://github.com/milisp/mcp-linker/releases/download/v#{version}/MCPLinker_#{version}_x64.dmg"
   end
 
   on_arm do
-    sha256 "dd9d2ae600b35a6e8237e0e9d42fa5683cb7a5465b4a633d1dfbd1443c9d0f5a"
+    sha256 "7fa7cb96feaf2ccfa8ecaee8b045db2304a9072026fd09a50435dcf085c660a6"
 
     url "https://github.com/milisp/mcp-linker/releases/download/v#{version}/MCPLinker_#{version}_aarch64.dmg"
   end
